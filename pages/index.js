6d879b6fc4642beff424e06667fe25d1f3c9eb43
Akshay Ramesh <akshayramesh92@gmail.com>
2022-04-03 01:42:17 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,6 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app! #brewwithakshay" />
         
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
